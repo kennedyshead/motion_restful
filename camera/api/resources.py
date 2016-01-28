9812f55581c1dbad24c64d0a5364eb5271b2a5c3
magnusknutas <magnus@thefarm.se>
2016-01-28 21:19:31 +0100

    mqtt for switching

--- camera/api/resources.py
+++ camera/api/resources.py
@@ -2,47 +2,25 @@
 # -*- coding: utf-8 -*-
 import json
 
-import requests
 from bs4 import BeautifulSoup
-from requests.auth import HTTPBasicAuth
-from settings import AUTH, MOTION_URL
+from camera.camera import Cam
+from settings import MOTION_URL, AUTH
 
 __author__ = 'magnusknutas'
 
-STATUS_URL = MOTION_URL + '%i/detection/status'
-PAUSE_URL = MOTION_URL + '%i/detection/pause'
-START_URL = MOTION_URL + '%i/detection/start'
 
 class CamResource:
+
+    camera = Cam(MOTION_URL, AUTH.get('name', None), AUTH.get('pass', None))
+
     def on_get(self, req, resp):
-        id = req.get_param_as_int('id', False)
+        cam_id = req.get_param_as_int('id', False)
 
-        if id:
-            if AUTH:
-                camera_status = requests.get(
-                        STATUS_URL % id,
-                        auth=HTTPBasicAuth(AUTH.get('name'), AUTH.get('pass'))
-                )
-            else:
-                camera_status = requests.get(
-                        STATUS_URL % id,
-                )
-
-            soup = BeautifulSoup(camera_status.content, 'html.parser')
-            status = soup.find('body').text.split('status')[1].strip()
-            resp.body = 'ON' if status == 'ACTIVE' else 'OFF'
-
+        if cam_id:
+            self.camera.get_state(cam_id)
+            resp.body = self.camera.get_state(cam_id)
         else:
-            if AUTH:
-                cameras = requests.get(
-                        MOTION_URL,
-                        auth=HTTPBasicAuth(AUTH.get('name'), AUTH.get('pass'))
-                )
-            else:
-                cameras = requests.get(
-                        MOTION_URL,
-                )
-
+            cameras = self.camera.list_cameras_no_parse()
             soup = BeautifulSoup(cameras.content, 'html.parser')
             cams = []
             for link in soup.find_all('a'):
@@ -54,41 +32,13 @@ class CamResource:
             resp.body = json.dumps(cams)
 
     def on_post(self, req, resp):
-        id = req.get_param_as_int('id', False)
-        if id:
-            if AUTH:
-                camera_status = requests.get(
-                        STATUS_URL % id,
-                        auth=HTTPBasicAuth(AUTH.get('name'), AUTH.get('pass'))
-                )
-            else:
-                camera_status = requests.get(
-                        STATUS_URL % id,
-                )
-
-            soup = BeautifulSoup(camera_status.content, 'html.parser')
-            sta = soup.find('body').text.split('status')[1].strip()
-            status = True if sta == 'ACTIVE' else False
+        cam_id = req.get_param_as_int('id', False)
+        if cam_id:
+            status = self.camera.get_state()
 
             if status:
-                if AUTH:
-                    requests.get(
-                            PAUSE_URL % id,
-                            auth=HTTPBasicAuth(AUTH.get('name'), AUTH.get('pass'))
-                    )
-                else:
-                    requests.get(
-                            PAUSE_URL % id
-                    )
+                self.camera.pause(cam_id)
                 resp.body = 'OFF'
             else:
-                if AUTH:
-                    requests.get(
-                            START_URL % id,
-                            auth=HTTPBasicAuth(AUTH.get('name'), AUTH.get('pass'))
-                    )
-                else:
-                    requests.get(
-                            START_URL % id
-                    )
+                self.camera.start(cam_id)
                 resp.body = 'ON'
